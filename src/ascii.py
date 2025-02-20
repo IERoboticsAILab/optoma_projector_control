4457f3993c2ca87a1a576e6b4ed0750f8a5f9e0a
farah orfaly <farah.orfaly@gmail.com>
2025-02-20 23:59:27 +0100

    Adding status checks after every command

--- src/ascii.py
+++ src/ascii.py
@@ -3,15 +3,9 @@ import serial
 import json
 import glob
 
-def find_usb_serial_device():
-    usb_devices = glob.glob('/dev/ttyUSB*')
-    if not usb_devices:
-        raise RuntimeError("No USB serial device found")
-    return usb_devices[0]  # Return first available USB device
+TOPIC_PREFIX = "home/projectors"
 
-print(find_usb_serial_device())
-ser = serial.Serial(find_usb_serial_device(), 9600)  
-
+# Commands for the projector
 commands_dict_ascii = {
     # Static commands (no parameters) 
     "HDMI1": "~00305 1\r", # HDMI1 
@@ -39,17 +33,29 @@ commands_dict_ascii = {
     "V-KEYSTONE": "~0066 n\r", # vertical keystone (-40 <= n <= 40)
 }
 
+# Find the USB serial device
+def find_usb_serial_device():
+    usb_devices = glob.glob('/dev/ttyUSB*')
+    if not usb_devices:
+        raise RuntimeError("No USB serial device found")
+    return usb_devices[0]  # Return first available USB device
+
+# Subscribe to the topic
 def on_subscribe(client, userdata, mid, granted_qos):
     print(f"Subscription successful with QoS: {granted_qos}")
 
+# Unsubscribe from the topic
 def on_unsubscribe(client, userdata, mid):
     print("Successfully unsubscribed.")
     client.disconnect()
 
+# Message received from the topic
 def on_message(client, userdata, msg):
     payload = msg.payload.decode('utf-8')
     received_message = json.loads(payload)
+
     print(f"Received message: {received_message} on topic: {msg.topic}")
+
     target = received_message['target']
     command = received_message['command']
     value = received_message.get('value')
@@ -61,16 +67,24 @@ def on_message(client, userdata, msg):
         if target == 'NORTH':
             ser.write(ascii_command.encode('utf-8'))
             print(f"Sent {received_message} command: {ascii_command}")
+            client.publish(f"{TOPIC_PREFIX}/status", json.dumps({"status": "OK", "command": command}))
+            print(f"Command {command} executed successfully")
     else:
         print(f"No command found for message: {command}")
+        client.publish(f"{TOPIC_PREFIX}/status", json.dumps({"status": "ERROR", "command": command}))
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print("Connected successfully.")
-        client.subscribe("home/projectors")
+        client.subscribe(f"{TOPIC_PREFIX}/#")
     else:
         print(f"Failed to connect, error code: {rc}")
 
+# Find the USB serial device
+print(find_usb_serial_device())
+ser = serial.Serial(find_usb_serial_device(), 9600)  
+
+# Initialize the MQTT client
 mqttc = mqtt.Client()
 mqttc.on_connect = on_connect
 mqttc.on_message = on_message
